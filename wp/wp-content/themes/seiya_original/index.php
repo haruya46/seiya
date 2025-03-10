1543436e2e9c43a7f3e6e390c92dc5c6a11d829c
平山晴也 <puzu70@gmail.com>
2025-03-10 23:55:21 +0900

    2025/3/10ワードプレス構築中

---
+++ wp/wp-content/themes/seiya_original/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    
+    <?php wp_footer(); ?>
+</body>
+</html>
